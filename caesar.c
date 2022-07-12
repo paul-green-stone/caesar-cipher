d37f56fd3d059801344d3e93573df34d5ad8f99d
paul-green-stone <stone.paul.green@gmail.com>
2022-07-12 20:28:58 +0300

    Fixed according to the encode/decode algorithm

--- caesar.c
+++ caesar.c
@@ -2,18 +2,19 @@
 
 char* caesarCipher_encrypt(char* const plaintext, size_t key) {
     char* ciphertext = NULL;
-    key = key % 26;
+
+    key %= 26;
 
     if ((plaintext != NULL) && (strlen(plaintext) != 0)) {
         if ((ciphertext = (char*) malloc(sizeof(char) * strlen(plaintext) + 1)) != NULL) {
             for (size_t i = 0; *(plaintext + i) != '\0'; i++) {
                 // For upper case letters
                 if ((*(plaintext + i) >= 'A') && (*(plaintext + i) <= 'Z'))
-                    *(ciphertext + i) = (*(plaintext + i) + key > 'Z') ? 'A' + (*(plaintext + i) + key - 'A') % 26 : *(plaintext + i) + key;
+                    *(ciphertext + i) = (*(plaintext + i) + key > 'Z') ? (*(plaintext + i) + key) - 26 : *(plaintext + i) + key;
 
                 //For lower case letters
                 else if ((*(plaintext + i) >= 'a') && (*(plaintext + i) <= 'z'))
-                    *(ciphertext + i) = (*(plaintext + i) + key > 'z') ? 'a' + (*(plaintext + i) + key - 'a') % 26 : *(plaintext + i) + key;
+                    *(ciphertext + i) = (*(plaintext + i) + key > 'z') ? (*(plaintext + i) + key) - 26 : *(plaintext + i) + key;
 
                 // For other characters
                 else
@@ -29,18 +30,18 @@ char* caesarCipher_encrypt(char* const plaintext, size_t key) {
 
 char* caesarCipher_decrypt(char* const ciphertext, size_t key) {
     char* plaintext = NULL;
-    key = key % ('z' - 'a');
+    key %= 26;
 
     if ((ciphertext != NULL) && (strlen(ciphertext) != 0)) {
         if ((plaintext = (char*) malloc(sizeof(char) * strlen(ciphertext) + 1)) != NULL) {
             for (size_t i = 0; *(ciphertext + i) != '\0'; i++) {
                 // For upper case letters
                 if ((*(ciphertext + i) >= 'A') && (*(ciphertext + i) <= 'Z'))
-                    *(plaintext + i) = (*(ciphertext + i) - key < 'A') ? 1 + 'Z' - ('A' - (*(ciphertext + i) - key)) % 26 : *(ciphertext + i) - key;
+                    *(plaintext + i) = (*(ciphertext + i) - key < 'A') ? (*(ciphertext + i) - key) + 26 : *(ciphertext + i) - key;
 
                 // For lower case letters
                 else if ((*(ciphertext + i) >= 'a') && (*(ciphertext + i) <= 'z'))
-                    *(plaintext + i) = (*(ciphertext + i) - key < 'a') ? 1 + 'z' - ('a' - (*(ciphertext + i) - key)) % 26 : *(ciphertext + i) - key;
+                    *(plaintext + i) = (*(ciphertext + i) - key < 'a') ? (*(ciphertext + i) - key) + 26 : *(ciphertext + i) - key;
 
                 // For other characters
                 else
--- main.c
+++ main.c
@@ -5,8 +5,11 @@ int main(int argc, char **argv) {
     char* str = "AAA aaa";
     char* str1 = "ZZZ zzz";
 
-    for (size_t i = 0; i < 27; i++)
-        printf("%llu - %s\n", i, caesarCipher_decrypt(str1, i));
+    for (size_t i = 0; i < 26; i++)
+        printf("%llu - %s\n", i, caesarCipher_decrypt(str, i));
+
+    for (size_t i = 0; i < 26; i++)
+        printf("%llu - %s\n", i, caesarCipher_encrypt(str1, i));
 
     return EXIT_SUCCESS;
 }
