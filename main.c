041fa553e9ee46716d9ee3230f0e60934b0a2ab7
paul-green-stone <stone.paul.green@gmail.com>
2022-07-16 00:32:30 +0300

    Added a cli encoder

--- main.c
+++ main.c
@@ -1,15 +1,59 @@
 #include "caesar.h"
+#include <errno.h>
+#include <getopt.h>
 
 int main(int argc, char **argv) {
 
-    char* str = "AAA aaa";
-    char* str1 = "ZZZ zzz";
+    // Global variables defined within the API:
+    extern char* optarg;
+    extern int optind, opterr, optopt;
+    extern int errno;
 
-    for (size_t i = 0; i < 26; i++)
-        printf("%llu - %s\n", i, caesarCipher_decrypt(str, i));
+    char* optstring = ":m:k:h";
+    char* user_message = NULL;
+    char* cipher = NULL;
+    size_t key;
 
-    for (size_t i = 0; i < 26; i++)
-        printf("%llu - %s\n", i, caesarCipher_encrypt(str1, i));
+    // Parsing the command line arguments
+    int opt;
+
+    while ((opt = getopt(argc, argv, optstring)) != -1) {
+        switch (opt) {
+            case 'h':
+                printf("usage: %s -m message/plaintext -k key\n", argv[0]);
+
+                break ;
+            case 'm':
+                if ((user_message = (char*) malloc(sizeof(char) * strlen(optarg) + 1)) != NULL)
+                    memcpy(user_message, optarg, strlen(optarg) + 1);
+
+                break ;
+            case 'k':
+                key = (size_t) atoi(optarg);
+
+                break ;
+            case '?':
+                printf("Unknown option: -%c\n", optopt);
+
+                break ;
+            case ':':
+                printf("Missing argument for -%c\n", optopt);
+
+                break ;
+        }
+    }
+
+    if (user_message != NULL) {
+        if ((cipher = caesarCipher_encrypt(user_message, key)) != NULL) {
+            printf("%s\n", cipher);
+
+            free(cipher);
+        }
+        else {
+            fprintf(stderr, "Error encrypting a message: %s\n", strerror(errno));
+            return EXIT_FAILURE;
+        }
+    }
 
     return EXIT_SUCCESS;
 }
